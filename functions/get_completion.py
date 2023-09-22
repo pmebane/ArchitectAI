38fab9e9292c6ec255a05b5401a32794580eb415
pmebane <142755986+pmebane@users.noreply.github.com>
2023-09-22 15:14:29 -0600

    Update get_completion.py

--- functions/get_completion.py
+++ functions/get_completion.py
@@ -1,8 +1,8 @@
-from config import OPENAI_API_KEY
+import streamlit as st
 import openai
 
 def get_completion(prompt, model="gpt-4", temperature=0):
-    openai.api_key  = OPENAI_API_KEY
+    openai.api_key  = st.secrets["db_username"]
     messages = [{"role": "user", "content": prompt}]
     response = openai.ChatCompletion.create(
         model=model,
