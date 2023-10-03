256ce418dfc30fe5937a55bf038dfd07000675c6
Peter Mebane <petermebane@Peters-MacBook-Air.local>
2023-10-02 19:56:11 -0600

    changed POC prompts

--- pages/poc.py
+++ pages/poc.py
@@ -10,18 +10,24 @@ if st.session_state.status != "Summary Complete":
     st.write("This will populate after your summary is generated")
 
 if st.session_state.status == "Summary Complete":
-    prompt = create_generate_poc_plan_prompt(st.session_state.messages[1:])
-    result = get_completion(prompt=prompt)
-    st.write(result)
-    #st.header("Project Goals")
-    #create summary of the goals of the project
-    #st.header("Project Requirements")
-    # create list of the requirements of the project
-    #st.header("Evaluation Plan")
-    #for tool in tool_list:
-        #   st.subheader(tool)
-        #create matrix of tools and requirements
-        #create plan for testing 
+    st.header("Project Goals")
+    with st.spinner("Generating..."):
+        prompt = create_generate_poc_goals_prompt(st.session_state.messages[1:]) # create summary of the goals of the project
+        project_goals = get_completion(prompt=prompt)
+    st.write(project_goals)
+    st.header("Project Requirements")
+    with st.spinner("Generating..."):
+        prompt = create_generate_poc_requirements_prompt(st.session_state.messages[1:]) # create list of the requirements of the project
+        project_requirements = get_completion(prompt=prompt)
+    st.write(project_requirements)
+    st.header("Evaluation Plan")
+    i = 0
+    for tool in st.session_state.tech_recommendations:
+        st.subheader(tool)
+        with st.spinner("Generating..."):
+            prompt = create_generate_poc_eval_prompt(st.session_state.vendor_recommendations[i], project_requirements) # create evaluation plan for each group of technologies
+            evaluation_plan = get_completion(prompt=prompt)
+        st.write(evaluation_plan)
 
     
 
--- pages/summary.py
+++ pages/summary.py
@@ -16,30 +16,17 @@ if st.session_state.status == "Conversation in Progress":
 # Conversation complete, now generate assets
 if st.session_state.status != "Conversation in Progress":
     st.subheader("Architecture Overview")
-
-    if "summary" not in st.session_state.keys():
-        with st.spinner("Generating..."):
-            # generate written description of architecture
-            generate_summary_prompt = create_generate_summary_prompt(st.session_state.messages[1:])
-            summary = get_completion(generate_summary_prompt)
-            st.session_state.summary = summary
-    else:
-        summary = st.session_state.summary
-        st.session_state.status = "Summary Complete"
+    with st.spinner("Generating..."):
+        generate_summary_prompt = create_generate_summary_prompt(st.session_state.messages[1:]) # generate written description of architecture
+        summary = get_completion(generate_summary_prompt)
     st.write(summary)
 
-
     st.subheader("Architecture diagram")
-    if "arch_url" not in st.session_state.keys():
-        with st.spinner("Generating..."):
-                # generate architecture diagram
-            generate_diagram_prompt = create_generate_diagram_prompt(summary)
-            mermaid_code = get_completion(generate_diagram_prompt)
-            mermaid_code = mermaid_code.replace("`","").replace("mermaid","")
-            url = get_arch_diagram(mermaid_code)
-            st.session_state.arch_url = url
-    else:
-        url = st.session_state.arch_url
+    with st.spinner("Generating..."):
+        generate_diagram_prompt = create_generate_diagram_prompt(summary) # generate architecture diagram
+        mermaid_code = get_completion(generate_diagram_prompt)
+        mermaid_code = mermaid_code.replace("`","").replace("mermaid","")
+        url = get_arch_diagram(mermaid_code)
     st.image(image=url)
 
     # extract recommendations
@@ -77,31 +64,36 @@ if st.session_state.status != "Conversation in Progress":
     "required": ["recommendations"]
     }
 
-    # call API and extract recommendations
+    # create section with information for each tool that was recommended
     st.subheader("Recommended Tools")
-    if "recommendations" not in st.session_state.keys():
-        with st.spinner("Generating..."):
-            prompt = create_extract_tech_prompt(st.session_state.messages[1:])
-            result = get_completion_with_function(prompt=prompt,function_schema=function_schema)
-            res = json.loads(result["choices"][0]["message"]["function_call"]["arguments"].replace("\n", ""))["recommendations"]
-            st.session_state.recommendations = res
-    else:
-        res = st.session_state.recommendations
-    for tech in res:
-        with st.expander(tech["tech"]):
+    with st.spinner("Generating..."):
+        prompt = create_extract_tech_prompt(st.session_state.messages[1:])
+        recommendations = get_completion_with_function(prompt=prompt,function_schema=function_schema)
+        cleaned_recommendations = json.loads(recommendations["choices"][0]["message"]["function_call"]["arguments"].replace("\n", ""))["recommendations"]
+    tech_list = [] # create list of the different technologies that were recommended
+    vendor_list = [] # create list of the different vendors that were recommended   
+    for tech in cleaned_recommendations:
+        tech_name = tech["tech"]
+        tech_list.append(tech_name) # add technology to the list
+        with st.expander(tech_name):
             tech_reasoning = tech["reasoning"]
-            st.write(tech_reasoning)
-            vendor_list = []
+            st.write(tech_reasoning) 
+            vendor_list_2 = [] # create a list of vendors for each technology
             for vendors in tech["vendors"]:
                 vendor = vendors["name"]
-                vendor_list.append(vendor)
+                vendor_list_2.append(vendor) # add individual vendor to the vendor list
                 vendor_reasoning = vendors["reasoning"]
                 st.write(f"**{vendor}:** {vendor_reasoning}")
+            vendor_list.append(vendor_list_2) # create list of vendor lists
             st.subheader("How You Choose")
             with st.spinner("Generating..."):
                 prompt = create_vendor_comparison_prompt(vendor_list)
-                result = get_completion(prompt=prompt)
-                st.session_state.comparison = result
-            st.write(result)
+                comparison = get_completion(prompt=prompt)
+            st.write(comparison) 
 
+    st.session_state.summary = summary
+    st.session_state.arch_url = url
+    st.session_state.recommendations = cleaned_recommendations
+    st.session_state.tech_recommendations = tech_list
+    st.session_state.vendor_recommendations = vendor_list
     st.session_state.status = "Summary Complete"
