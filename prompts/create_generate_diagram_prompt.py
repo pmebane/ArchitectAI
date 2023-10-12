bf62477b0e2362e119c50dc9be679490badf8410
Peter Mebane <petermebane@peters-air.myfiosgateway.com>
2023-10-12 18:47:35 -0400

    changed summary and arch diagram prompts

--- prompts/create_generate_diagram_prompt.py
+++ prompts/create_generate_diagram_prompt.py
@@ -1,6 +1,10 @@
 def create_generate_diagram_prompt(summary):
-    prompt = f"""Look at the data architecture delimited by triple backticks. Generate the Mermaid code needed to create a diagram for this architecture. \
-    Only generate code, do not include anything else in your response. In the code, all lines after the first should be indented. \
+    prompt = f"""You are an expert in Mermaid code.
+    You have been given a summary of a data architecture. The summary is delimited by triple backticks.
+
+    Generate the mermaid code that would display a diagram for this archicture. The mermaid code should depict how the tools interact with one another. It should not include the names of languages or cloud platforms.
+    
+    Only generate code, do not include anything else in your response. In the code, all lines after the first should be indented. 
     All node labels should be enclosed in square brackets and double quotes. For example, if you wanted node A to display `[Fe(CN)6]4-`, it should be written as `A["[Fe(CN)6]4-"]`
     
     '''
--- prompts/create_generate_summary_prompt.py
+++ prompts/create_generate_summary_prompt.py
@@ -1,6 +1,13 @@
 def create_generate_summary_prompt(chat_history):
-    prompt = f"""Look at the chat history delimited by triple backticks. Generate a description of how the tools that were recommended would integrate in a data architecture.
-    Do not include the names of individual vendors in your response. You should only include names of technology groups like Data Warehouse, Streaming Ingestion, or Visualization.
+    prompt = f"""You are an expert in data architecture.
+    
+    You have been given a conversation between a user and an assistant. The conversation is delimited by triple backticks.
+    
+    The assistant recommended tools to the user that they should use in their data architecture. You should generate a description of how the tools that were recommended would integrate in a data architecture.
+    
+    Do not include the names of individual vendors in your response or discuss the requirements of the tools. 
+    
+    You should only include names of technology groups like Data Warehouse, Streaming Ingestion, or Visualization.
     
     '''
     {chat_history}
